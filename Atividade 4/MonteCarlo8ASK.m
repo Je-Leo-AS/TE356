48b9e137fd708164bb7ead30ecc23c0be5f4730b
Leo <leonardo_as20@hotmail.com>
2023-04-19 14:46:54 -0300

    atualização 8ask

--- Atividade 4/MonteCarlo8ASK.m
+++ Atividade 4/MonteCarlo8ASK.m
@@ -1,20 +1,26 @@
 function ser = MonteCarlo8ASK(EbN0, numIter, N)
     ser = zeros(size(EbN0));
-    for k = 1:length(EbN0)
+    symb = [1 1 1;            1 1 -1;            1 -1 1;            1 -1 -1;           -1 1 1;           -1 1 -1;           -1 -1 1;           -1 -1 -1];
+    for k=1:length(EbN0)
         erro = 0;
         erro_total = zeros(size(N));
-        for i = 1:numIter
-            bits = randi([0 7],1,N); % gerar 3 bits por símbolo (8-ASK)
-            symb = 2*bits - 7; % mapear para símbolos polar 8-ASK
+        for i=1:numIter
+            bits = randi([0 1],1,3*N);
+            bits_reshape = reshape(bits, 3, [])';
+            deci = bi2de(bits_reshape);
+            symb_tx = symb(deci+1,:);
+            symb = symb./sqrt(3); % normalização para que os símbolos tenham energia 1
             SNR = 10^(EbN0(k)/10);
-            sigma_sq = 1/(3*SNR); % 3 bits por símbolo (8-ASK)
-            n = sqrt(sigma_sq)*(randn(1,N)+1i*randn(1,N));
-            r = symb + n;
-            bitsRx = floor((real(r)+4)/2); % demapear para bits 3 bits por símbolo (8-ASK)
-            erro = sum(bits~=bitsRx)/N;
+            sigma_sq = 1/(3*SNR);
+            n = sqrt(sigma_sq)*(randn(N,3)+1i*randn(N,3));
+            r = symb_tx + n;
+            [~, deci_rx] = min(abs(r - repmat(symb_tx, 1, 1, N)), [], 2);
+            bitsRx = de2bi(deci_rx-1);
+            bitsRx_reshape = reshape(bitsRx', 1, [])';
+            erro = sum(bits~=bitsRx_reshape)/(3*N);
             erro_total(i) = erro;
         end
-        ser(k) = mean(erro_total)/3; % dividir por 3 para obter SER
+        ser(k) = mean(erro_total);
     end
 end
 
--- Atividade 4/comandos.m
+++ Atividade 4/comandos.m
@@ -5,10 +5,8 @@ N = 10^5;
 ber = MonteCarlo2ASK(EbN0, numIter, N);
 hold on
 plot(EbN0,ber)
-EbN0 = [0 5 10 15];
-ser8 = MonteCarlo8ASK(EbN0, numIter, N);
-ber1 = ser8/log2(8);
-plot(EbN0,ber1)
+
+
 EbN0 = [0 5 10 12];
 ser16 = MonteCarlo16QAM(EbN0, numIter, N);
 ber2 = ser16/log2(16);
