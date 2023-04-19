39efd4cb820b4a7c018c738319582c3c6750185c
je <leonardo_as20@hotmail.com>
2023-04-19 17:14:26 -0300

    correção 8ask

--- Atividade 4/MonteCarlo8ASK.m
+++ Atividade 4/MonteCarlo8ASK.m
@@ -1,26 +1,25 @@
 function ser = MonteCarlo8ASK(EbN0, numIter, N)
     ser = zeros(size(EbN0));
-    symb = [1 1 1;            1 1 -1;            1 -1 1;            1 -1 -1;           -1 1 1;           -1 1 -1;           -1 -1 1;           -1 -1 -1];
+    const = [-7 -5 -3 -1 1 3 5 7];
+    Es = mean(const.^2);
+    symb = const/sqrt(Es);
     for k=1:length(EbN0)
         erro = 0;
-        erro_total = zeros(size(N));
+        SNR = 10^(EbN0(k)/10);
+        sigma_sq = 1/(2*SNR);
+        sigma = sqrt(sigma_sq);
         for i=1:numIter
-            bits = randi([0 1],1,3*N);
-            bits_reshape = reshape(bits, 3, [])';
-            deci = bi2de(bits_reshape);
-            symb_tx = symb(deci+1,:);
-            symb = symb./sqrt(3); % normalização para que os símbolos tenham energia 1
-            SNR = 10^(EbN0(k)/10);
-            sigma_sq = 1/(3*SNR);
-            n = sqrt(sigma_sq)*(randn(N,3)+1i*randn(N,3));
-            r = symb_tx + n;
-            [~, deci_rx] = min(abs(r - repmat(symb_tx, 1, 1, N)), [], 2);
-            bitsRx = de2bi(deci_rx-1);
-            bitsRx_reshape = reshape(bitsRx', 1, [])';
-            erro = sum(bits~=bitsRx_reshape)/(3*N);
-            erro_total(i) = erro;
+           ind = randi(8);
+           simbolo_constelacao = symb(ind);
+           noise = randn * sigma;
+           simbolo_noise = simbolo_constelacao + noise;
+           [v,simbolo_recebido] = min(abs(const - simbolo_noise));
+
+           if (simbolo_recebido ~= simbolo_constelacao)
+             erro = erro + 1;
+           endif
         end
-        ser(k) = mean(erro_total);
+        ser(k) = erro/numIter;
     end
 end
 
